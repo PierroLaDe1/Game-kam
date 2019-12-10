31bd69ec5cb9d85141248b7744c623a32e5daf59
mariine <marine.bouchet@lecole-ldlc.com>
2019-12-10 17:44:40 +0100

    salle finis

---
+++ Assets/FPS/Scripts/target.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class target : MonoBehaviour
+
+{
+    void Start()
+    {
+        InvokeRepeating("LaunchProjectile", 1f, 0.1f);
+    }
+
+    Vector3 vectorDirection = new Vector3(0f, 0f, 1f);
+    void LaunchProjectile()
+    { 
+      if (Physics.Raycast(transform.position, vectorDirection, 2f, LayerMask.GetMask("Well")))
+        {
+            vectorDirection = -vectorDirection;
+        }
+        else
+        {
+            transform.position += vectorDirection;
+        }
+    }
+}
