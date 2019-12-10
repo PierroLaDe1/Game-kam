401fdb0488438e2e4949ce733d7dc51b26f98ffc
Pierre <stadelmannpierre@yahoo.com>
2019-12-10 17:16:58 +0100

    Debut du jeu

---
+++ Assets/NameTransfert.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameTransfert : MonoBehaviour
+{
+    public string theName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+    public GameObject Button;
+    public void StoreName()
+    {
+        theName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Bienvenue " + theName + ", te voila dans notre parc ! prends l'arme , détruit toutes les cibles et découvres la suite ! ";
+        Button.SetActive(true);
+    }
+    
+    
+}
