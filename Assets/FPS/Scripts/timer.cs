f38fdbc7cad609c45df58e1d7af50ef0008c9a67
Thomas <thomas.girard@lecole-ldlc.com>
2019-12-12 11:00:27 +0100

    oiuf

---
+++ Assets/FPS/Scripts/timer.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class timer : MonoBehaviour
+{
+public float temps;
+public Text textComponent;
+
+public float Timerinterval = 5f;
+// Start is called before the first frame update
+void Start()
+{
+//On met le texte à jour pour afficher le nombre de vies
+textComponent.text = temps.ToString();
+}
+
+void Update()
+{
+        
+//On enlève une vie
+temps -= Time.deltaTime;
+//On met le texte à jour
+textComponent.text = temps.ToString();
+    if (temps <0  )
+Debug.Log("PERDU ");
+    
+}
+}
